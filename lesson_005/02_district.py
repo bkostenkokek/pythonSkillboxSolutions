c1e47e57b25331ecf2d97c01fb844cb2c9959f17
Bogdan Kostenko <bogdankostenko83@gmail.com>
2022-04-20 15:31:57 +0300

    fix task

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -8,10 +8,11 @@
 # TODO здесь ваш код
 from district.central_street.house1 import room1 as cs_r1, room2 as cs_r2
 from district.central_street.house2 import room1 as cs_r1_1, room2 as cs_r2_1
+from district.soviet_street.house1 import room1 as ss_r1, room2 as ss_r2
+from district.soviet_street.house2 import room1 as ss_r1_2, room2 as ss_r2_2
+
 print('На районе central_street живут -', ', '.join(cs_r1.folks), ', '.join(cs_r2.folks), ', '.join(cs_r1_1.folks),
       ', '.join(cs_r2_1.folks))
 
-from district.soviet_street.house1 import room1 as ss_r1, room2 as ss_r2
-from district.soviet_street.house2 import room1 as ss_r1_2, room2 as ss_r2_2
 print('На районе soviet_street живут -', ', '.join(ss_r1.folks), ', '.join(ss_r2.folks), ', '.join(ss_r1_2.folks),
       ', '.join(ss_r2_2.folks))
