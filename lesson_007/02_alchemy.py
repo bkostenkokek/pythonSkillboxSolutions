f215591aef799fdef8843983cdb6dc84652e2ca5
Bogdan Kostenko <bogdankostenko83@gmail.com>
2022-04-27 17:12:31 +0300

    Game version number 1

--- lesson_007/02_alchemy.py
+++ lesson_007/02_alchemy.py
@@ -20,6 +20,110 @@
 #   print(Fire(), '+', Air(), '=', Fire() + Air())
 
 # TODO здесь ваш код
+class Water:
+    def __str__(self):
+        return "Вода"
+
+    def __add__(self, other):
+        if isinstance(other, Air):
+            return Storm()
+        elif isinstance(other, Fire):
+            return Vapor()
+        elif isinstance(other, Earth):
+            return Dirt()
+        else:
+            None
+
+
+class Air:
+    def __str__(self):
+        return "Воздух"
+
+    def __add__(self, other):
+        if isinstance(other, Fire):
+            return Lightning()
+        elif isinstance(other, Earth):
+            return Dust()
+        else:
+            None
+
+
+class Fire:
+    def __str__(self):
+        return "Огонь"
+
+    def __add__(self, other):
+        if isinstance(other, Earth):
+            return Magma()
+        else:
+            None
+
+
+class Earth:
+    def __str__(self):
+        return "Земля"
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Dirt()
+        elif isinstance(other, Air):
+            return Dust()
+        elif isinstance(other, Fire):
+            return Magma()
+        else:
+            None
+
+
+class Storm:
+    def __str__(self):
+        return "Шторм"
+
+
+class Vapor:
+    def __str__(self):
+        return "Пар"
+
+
+class Dirt:
+    def __str__(self):
+        return "Грязь"
+
+
+class Lightning:
+    def __str__(self):
+        return "Молния"
+
+
+class Dust:
+    def __str__(self):
+        return "Пыль"
+
+
+class Magma:
+    def __str__(self):
+        return "Лава"
+
+
+water = Water()
+air = Air()
+earth = Earth()
+fire = Fire()
+
+print(water + air)
+print(water + earth)
+print(water + fire)
+
+
+# element_user_1 = input("Введите елемент №1: ")
+# element_user_2 = input("Введите елемент №2: ")
+# if element_user_1 == "Water" and element_user_2 == "Air":
+#     print("Вы создали Шторм")
+# else:
+#     print("Вы ввели неверные елементы")
+#
+#
+
+
 
 # Усложненное задание (делать по желанию)
 # Добавить еще элемент в игру.
